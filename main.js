c5df98717d072bf7c1d8e581fb5b1f48664cd8e1
Jesús Costa Gálvez <jcostagalvez@gmail.com>
2020-04-12 01:11:58 +0200

    second commit

--- main.js
+++ main.js
@@ -4,5 +4,31 @@ const app = new Vue ({
         product:'Socks',
         image:'./resource/vmSocks-green-onWhite.jpg',
         onStock: true,
+        details:['80% Cotton', '20% Polyester', 'Gender-neutral'],
+        variants : [
+            {
+                variantId:2234 ,
+                variantColor: 'green',
+                variantImage: './resource/vmSocks-green-onWhite.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: './resource/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+
+        cart: 0,
+    },
+
+    methods:{
+        addToCart() {
+            this.cart += 1
+        },
+
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+
     }
 })
